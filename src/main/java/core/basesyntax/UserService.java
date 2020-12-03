a2b1f87fc80507b70d4be25650c270f7179cf3b7
kerrender <agreed_note@hotmail.com>
2020-12-03 19:20:30 +0200

    redundant lines deleted

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -5,13 +5,13 @@ import core.basesyntax.exception.UserNotFoundException;
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-
+        String[] dividedRecord;
         for (int i = 0; i < records.length; i++) {
-            if (records[i].substring(0, records[i].indexOf(":")).equals(email)) {
-                return Integer.parseInt(records[i].substring(records[i].indexOf(':') + 1));
+            dividedRecord = records[i].split(":");
+            if (dividedRecord[0].equals(email)) {
+                return Integer.parseInt(dividedRecord[1]);
             }
         }
         throw new UserNotFoundException("User with given email doesn't exist");
-
     }
 }
