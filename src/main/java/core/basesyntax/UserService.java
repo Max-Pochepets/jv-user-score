803df5424b0cb7079653ed92cb28050cbdc90008
kerrender <agreed_note@hotmail.com>
2020-12-03 17:41:29 +0200

    method getUserScore came back to normal

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,13 +7,8 @@ public class UserService {
     public int getUserScore(String[] records, String email) {
 
         for (int i = 0; i < records.length; i++) {
-            if (records[i]
-                    .substring(0, records[i].indexOf(":"))
-                    .equals(email)) {
-                return Integer
-                        .parseInt(records[i]
-                                .substring(records[i]
-                                        .indexOf(':') + 1));
+            if (records[i].substring(0, records[i].indexOf(":")).equals(email)) {
+                return Integer.parseInt(records[i].substring(records[i].indexOf(':') + 1));
             }
         }
         throw new UserNotFoundException("User with given email doesn't exist");
