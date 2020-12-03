9824379253ed5e97015f5277600639050a599c9a
kerrender <agreed_note@hotmail.com>
2020-12-03 17:18:54 +0200

    initial

---
+++ src/main/java/core/basesyntax/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package core.basesyntax.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException() {
+        super("User with given email doesn't exist");
+    }
+
+}
